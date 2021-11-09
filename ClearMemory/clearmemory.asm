e0b55a528e702c68d68d40c4d95a484bd8840610
Tyler Taylor <61921198+TylerOliverTaylor@users.noreply.github.com>
2021-11-08 21:32:22 -0500

    Update clearmemory.asm
    Finished the code to clear memory.

--- ClearMemory/clearmemory.asm
+++ ClearMemory/clearmemory.asm
@@ -12,6 +12,18 @@ Start:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Clear the Page Zero region ($00 to $FF)
 ; Meaning the entire RAM and also the entire TIA registers
+; Meaning setting the values in all registers to 0
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     lda #0          ; A = 0
     ldx #$FF        ; X = #$FF
+MemLoop:
+    sta $0,X        ; Store the value of A inside memory address $0 + X
+    dex             ; X--
+    bne MemLoop     ; Loop until X is equal to zero (z-flag is set)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Fill the ROM size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    org $FFFC
+    .word Start     ; Reset vector at $FFFC (where the program starts)
+    .word Start     ; Interrupt vector at $FFFE (unused in the VCS)
